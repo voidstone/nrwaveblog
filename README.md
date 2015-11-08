24645eb36220b9b2d2db739b71e214edfc5959a9
Serzh Chaikin <chaikin.sergeygmail.comchaikin.sergeygmail.com>
2015-11-08 10:24:47 -0800

    first pages

---
+++ README.md
@@ -0,0 +1,4 @@
+nrwaveblog
+==========
+
+A Symfony project created on November 5, 2015, 10:28 am.
---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController extends Controller
+{
+    /**
+     * @Route("/lucky/number/{count}")
+     */
+    public function numberAction($count)
+    {
+        $numbers = array();
+        for ($i = 0; $i < $count; $i++) {
+            $numbers[] = rand(0, 100);
+        }
+        $numbersList = implode(', ', $numbers);
+
+        /*return new Response(
+            '<html><body>Lucky numbers: '.$numbersList.'</body></html>'
+        );*/
+		
+		return $this->render(
+			'lucky/number.html.twig',
+			  array('luckyNumberList' => $numbersList)
+			  );
+
+
+    }
+
+     /**
+     * @Route("/api/lucky/number")
+     */
+    public function apiNumberAction()
+    {
+        $data = array(
+            'lucky_number' => rand(0, 100),
+        );
+
+        return new Response(
+            json_encode($data),
+            200,
+            array('Content-Type' => 'application/json')
+        );
+    }
+
+
+}
